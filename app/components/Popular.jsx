6a018a748a38c9b876468b4fc4c0d370c7047b2a
hendri-27 <hendriantonius27@gmail.com>
2025-07-22 20:45:02 +0700

    feat: create repos table

--- app/components/Popular.jsx
+++ app/components/Popular.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import PropTypes from "prop-types";
 import { fetchPopularRepos } from "../utils/api";
+import Table from "./Table";
 
 function LanguagesNav({ selected, onUpdateLanguage }) {
   const languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python"];
@@ -78,7 +79,7 @@ export default class Popular extends React.Component {
         </div>
         {error && <p className="text-center error">{error}</p>}
 
-        {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
+        {repos && <Table repos={repos} />}
       </main>
     );
   }
