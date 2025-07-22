71560b324f14112783d7fdcbf0c03dd95d039ebd
hendri-27 <hendriantonius27@gmail.com>
2025-07-22 22:14:13 +0700

    feat: add battle component

--- app/index.jsx
+++ app/index.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import "./index.css";
 import Popular from "./components/Popular";
+import Battle from "./components/Battle";
 
 /*
   state
@@ -16,7 +17,7 @@ class App extends React.Component {
     return (
       <div className="light">
         <div className="container">
-          <Popular />
+          <Battle />
         </div>
       </div>
     );
