1145ff1bb01afc1763bbdb319ff02a6fbf607edd
hendri-27 <hendriantonius27@gmail.com>
2025-07-23 21:22:16 +0700

    feat: add withSearchParams

--- app/components/Battle.jsx
+++ app/components/Battle.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import PropTypes from "prop-types";
 import { close } from "./icons";
-import Results from "./Results";
+import { Link } from "react-router-dom";
 
 function Instructions() {
   return (
@@ -103,7 +103,6 @@ export default class Battle extends React.Component {
     this.state = {
       playerOne: null,
       playerTwo: null,
-      battle: false,
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -123,22 +122,19 @@ export default class Battle extends React.Component {
     const { playerOne, playerTwo, battle } = this.state;
     const disabled = !playerOne || !playerTwo;
 
-    if (battle === true) {
-      return <Results playerOne={playerOne} playerTwo={playerTwo} />;
-    }
-
     return (
       <main className="stack main-stack animate-in">
         <div className="split">
           <h1>Players</h1>
-          <button
-            className={`btn primary ${disabled ? "disabled" : ""}`}
-            onClick={() => {
-              this.setState({ battle: true });
+          <Link
+            to={{
+              pathname: "/results",
+              search: `?playerOne=${playerOne}&playerTwo=${playerTwo}`,
             }}
+            className={`btn primary ${disabled ? "disabled" : ""}`}
           >
             Battle
-          </button>
+          </Link>
         </div>
         <section className="grid">
           {playerOne === null ? (
