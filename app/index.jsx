67460a6cdecbcebd244feb13e4343a02e8664e99
hendri-27 <62458850+hendri-27@users.noreply.github.com>
2025-07-23 14:22:21 +0700

    feat: add tooltip

--- app/index.jsx
+++ app/index.jsx
@@ -17,7 +17,7 @@ class App extends React.Component {
     return (
       <div className="light">
         <div className="container">
-          <Battle />
+          <Popular />
         </div>
       </div>
     );
